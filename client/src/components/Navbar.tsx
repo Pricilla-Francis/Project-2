692252ae2c162f82b64c0150707c891e0b4f9708
Pricilla Francis <Pricilla@Pricillas-MacBook-Air.local>
2025-03-31 21:31:34 -0400

    home.css-update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -20,8 +20,8 @@ const Navbar = () => {
   }, [loginCheck]);  // Dependency array ensures useEffect runs when loginCheck changes
 
   return (
-    <div className="display-flex justify-space-between align-center py-2 px-5 mint-green">
-      <h1>
+    <div >
+      <h1 className='title-1'>
         Authentication Review
       </h1>
       <div>
